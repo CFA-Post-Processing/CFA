35576554bc4c693ca9e8ccb56eb7ef17e39de1c7
AzzurraS <45337743+AzzurraS@users.noreply.github.com>
2021-05-06 10:42:53 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -12,28 +12,37 @@ library(shiny)
 setwd("C:/Users/azzur/Dropbox/Il mio PC (LAPTOP-AUDRMQN9)/Desktop/CFA_data")
 source('CFA_data_GC.R') #source must be outside the ui and server function
 
-# User interface ----
-ui<- fluidPage(
-  # Application title
-  titlePanel("Continuous Flow Analysis"),
-  # Listing DFs
-  selectInput(
-    inputId = "tableName", label = "Sample:",
-    selected = NULL, multiple = FALSE,
-    choices = c("Select sample", SAMPLE)
-  ),
-  uiOutput("secondSelection")
+runApp(list(
+  ui = fluidPage(
+    titlePanel("CFA"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("sample",label = "sample",choices = c("BAG1", "BAG2")),
+        selectInput("data",label = "data",choices = "")
+      ),
+      mainPanel(
+        plotOutput("plot")
+      )
+    )
+  )
+  
+  
+  , server = function(input,output,session) {
+    observe({
+      if (input$sample == "BAG1") {
+        choices <- c("BAG1_abakus","BAG1_cond","BAG1_flow","BAG1_IceHeight","BAG1_MeltSpeed")
+      }
+      else {
+        choices <- c("BAG2_abakus","BAG2_cond","BAG2_flow","BAG2_IceHeight","BAG2_MeltSpeed",
+                     "Cond_NormSmooth_peakArea","Cond_NormSmooth_peaks","DELAYS")
+      }
+      updateSelectInput(session,"data",choices=choices,selected=choices[1])
+    })
+    
+    output$plot <- renderPlot({
+      if (input$data != "") {
+        plot(runif(as.numeric(input$data)),col=isolate(input$sample))
+      }
+    })
+  })
 )
-
-# Server logic ----
-server <- function(input, output){
-  output$secondSelection <- renderUI({
-    selectInput(
-      inputId = "tableName", label = "Data:",
-      selected = NULL, multiple = FALSE,
-      choices = c("choose", SAMPLE$samples)
-    )})
-}
-
-
-shinyApp(ui, server)
